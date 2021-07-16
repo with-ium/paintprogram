e929e575ef44a4b664c632f3cb821dc0dbf15b83
Wayne Gin Tan <wayne.g.tan@student.curtin.edu.au>
2021-07-16 20:14:21 +0800

    Paint program commit.

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+from .settings import *
+from .button import *
+import pygame
+pygame.init()
+pygame.font.init()
+
---
+++ utils/settings.py
@@ -0,0 +1,28 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+FPS = 240
+
+WIDTH, HEIGHT = 600, 700
+
+ROWS = COLS = 50
+
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+PIXEL_SIZE = WIDTH // COLS
+
+BG_COLOR = WHITE
+
+DRAW_GRID_LINES = True
+
+def get_font(size):
+    return pygame.font.SysFont("comicsans", size)
+
+
